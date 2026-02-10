9859aadccb774c49184fba98bf97e6097c1e8bac
Fernanda Lembo Vedovello <fe.vedovello@gmail.com>
2026-02-10 13:55:46 -0300

    Apenas sqs

--- infra/sqs.tf
+++ infra/sqs.tf
@@ -6,21 +6,3 @@ resource "aws_sqs_queue" "order_queue" {
   tags = var.tags
 }
 
-resource "aws_sqs_queue_policy" "order_queue_policy" {
-  queue_url = aws_sqs_queue.order_queue.id
-
-  policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Effect = "Allow"
-        Principal = {
-          Service = "sns.amazonaws.com"
-        }
-        Action   = "sqs:SendMessage"
-        Resource = aws_sqs_queue.order_queue.arn
-      }
-    ]
-  })
-}
-
